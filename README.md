f94333b2307ffb0ca706152b32b314159603cd1e
enriqueuz <uzcategui.systems@gmail.com>
2021-05-10 13:09:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Tweets downloader with Tweepy
+
+Basic tweet downloader with tweepy using the twitter API (configured to work with academic research)
+
+## Installation
+
+* Create a virtual env
+* Run ```pip install -r requirements```
+* Create and .env file with your API credentials
+
+## Usage
+
+* Run ``` python3 retrieves_tweets.py ```
+* Enter the word, phrase or hashtag you would like to retrieve tweets from 
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ retrieve_tweets.py
@@ -0,0 +1,36 @@
+import credentials
+import tweepy
+
+def retrieve_tweets(query, tweets_number):
+    auth = tweepy.OAuthHandler(credentials.API_KEY, credentials.API_SECRET_KEY)
+    auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+
+    api = tweepy.API(auth)
+
+    query = query.strip().lower()
+    filename = query.replace(" ", "_") + '.txt'
+
+    id = None
+    count = 0
+    while count <= tweets_number:
+        tweets = api.search(q=query, lang='es', tweet_mode='extended', max_id=id)
+
+        for tweet in tweets:
+            if tweet.full_text.startswith('RT'):
+                count += 1
+                continue
+            
+            f = open(filename, 'a', encoding='utf-8')
+            f.write(tweet.full_text + '\n')
+            count += 1
+            f.close
+        
+        id = tweet.id
+        print(count)
+
+if __name__ == '__main__':
+    query = input("Type a hashtag, word or phrase you would like like to "
+    "retrieve tweets from: ")
+    tweets_number = int(input("Enter the quantity of tweets you want to download "
+    "(max 3000): "))
+    retrieve_tweets(query, tweets_number)
